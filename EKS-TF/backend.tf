d0b9fa0f885f3f7230c491079ad1f0c17b85e092
Houcem khlifi <houcemkhlifi.devops@gmail.com>
2024-02-05 10:16:09 +0000

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
+    bucket         = "my-ews-baket3"
+    region         = "eu-west-3"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    #dynamodb_table = "Lock-Files"
     encrypt        = true
   }
   required_version = ">=0.13.0"
